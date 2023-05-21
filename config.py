1a0b0dd9562afaf683c4ec06df908e296066cdfd
kalebbroo <kalebbroo@gmail.com>
2023-05-21 02:55:28 -0400

    config me

---
+++ config.py
@@ -0,0 +1,6 @@
+# Replace these with your own API keys and credentials
+SPOTIFY_CLIENT_ID = 'your-spotify-client-id'
+SPOTIFY_CLIENT_SECRET = 'your-spotify-client-secret'
+SPOTIFY_REDIRECT_URI = 'http://localhost:8888/callback'
+TOKEN = 'your-discord-bot-token'
+ROLE = 'your-discord-role'
